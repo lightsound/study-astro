20c570868956ead3e2f02b8febea0c508ecaace4
しまぶー <0120koto@gmail.com>
2023-01-30 00:37:25 +0900

    🚀

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
-import netlify from "@astrojs/netlify/edge-functions";
+import netlify from "@astrojs/netlify/functions";
 
 export default defineConfig({
   output: "server",
