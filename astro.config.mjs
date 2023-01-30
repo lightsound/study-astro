fd2ee08a470e49b610fdd160d27f0e66cda54ca1
しまぶー <0120koto@gmail.com>
2023-01-30 15:01:25 +0900

    🚀

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/serverless";
 // import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [preact()],
-  adapter: vercel(),
+  adapter: vercel({
+    includeFiles: ["./src/content"],
+  }),
   // adapter: netlify(),
 });
